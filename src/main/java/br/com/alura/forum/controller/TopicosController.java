20dfd2f35e639d887f9a540ca741495202bf614a
YuriRomao1 <yurijj@hotmail.com>
2022-06-23 09:55:59 -0300

    Usando Spring Data

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,23 +1,28 @@
 package br.com.alura.forum.controller;
 
-import br.com.alura.forum.controller.dto.TopicoDTO;
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.dto.TopicoDto;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TopicosController {
 
-    @RequestMapping("/topicos")
-    @ResponseBody
-    public List<TopicoDTO> lista(){
-        var topico = new Topico("Duvida", "Duvida com Spring", new Curso("Spring", "Programação"));
+    @Autowired
+    private TopicoRepository topicoRepository;
 
-        return TopicoDTO.converter(Arrays.asList(topico, topico, topico));
+    @RequestMapping("/topicos")
+    public List<TopicoDto> lista(String nomeCurso) {
+        if (nomeCurso == null) {
+            List<Topico> topicos = topicoRepository.findAll();
+            return TopicoDto.converter(topicos);
+        } else {
+            List<Topico> topicos = topicoRepository.findByTitulo(nomeCurso);
+            return TopicoDto.converter(topicos);
+        }
     }
 }
--- src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
+++ src/main/java/br/com/alura/forum/controller/dto/TopicoDto.java
@@ -1,28 +1,27 @@
 package br.com.alura.forum.controller.dto;
 
 import br.com.alura.forum.modelo.Topico;
-import org.springframework.boot.jdbc.metadata.TomcatDataSourcePoolMetadata;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class TopicoDTO {
+public class TopicoDto {
 
     private Long id;
     private String titulo;
     private String mensagem;
     private LocalDateTime dataCriacao;
 
-    public TopicoDTO(Topico topico){
+    public TopicoDto(Topico topico){
         this.id = topico.getId();
         this.titulo = topico.getTitulo();
         this.mensagem = topico.getMensagem();
         this.dataCriacao = topico.getDataCriacao();
     }
 
-    public static List<TopicoDTO> converter(List<Topico> topicos) {
-        return topicos.stream().map(TopicoDTO::new).collect(Collectors.toList());
+    public static List<TopicoDto> converter(List<Topico> topicos) {
+        return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
 
     }
 
